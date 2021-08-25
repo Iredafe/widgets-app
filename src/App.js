567dd1c21f0d7b31fe120b07ee6a9d9850b734c6
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-08-24 22:29:09 -0600

    add the items prop

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 
+const items =[
+    {
+        title: 'What is React',
+        content: 'React is  a front end javascript framework'
+    },
+    {
+        title: 'Why use React?',
+        content: 'React is a favorite JS library'
+    },
+    {
+        title: 'How do you i=use React',
+        content: 'By creating reusable components'
+    }
+]
 
 export default ()=>{
     return (<div>
-        <Accordion/>
+        <Accordion items={items}/>
     </div>);        
 
 };
