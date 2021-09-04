3b2e84bc6cfcfceb4a5f40997e40a749827aad82
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-09-03 21:00:05 -0600

    make request

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,10 +4,14 @@ import React, {useState, useEffect} from 'react';
 
 const Search =()=>{
    const [term, setTerm]= useState('');
+    const [results, setResults] = useState([]);
+    console.log(results);
 
-   useEffect(()=>{
+
+
+    useEffect(()=>{
        const search = async ()=>{
-           await axios.get('https://en.wikipedia.org/w/api.php', {
+          const data= await axios.get('https://en.wikipedia.org/w/api.php', {
                params:{
                 action:'query',
                 list: 'search',
@@ -16,8 +20,12 @@ const Search =()=>{
                 srsearch: term
                }
            })
+           setResults(data);
        };
-       search();
+       if(term){
+        search();
+
+       }
     },
     [term]);
  return (
