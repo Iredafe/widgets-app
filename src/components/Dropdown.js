0f09f9dc60b140228971249341c0343fd04060ea
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-09-20 21:23:48 -0600

    hide and show list

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Dropdown = ({options, selected, onSelectionChange}) =>{
-
+const [open, setOpen]= useState(false);
     const renderedOptions = options.map((option)=>{
         if(option.value===selected.value){
             return null;
@@ -15,10 +15,10 @@ const Dropdown = ({options, selected, onSelectionChange}) =>{
     return (<div className="ui form">
         <div className="field">
         <label className="label">Select a Color</label>
-        <div className="ui selection dropdown visible active">
+        <div className={`ui selection dropdown ${open? 'visible active': ''}`}>
             <i className="dropdown icon"></i>
             <div className="text">{selected.label}</div>
-            <div className="menu visible transition">
+            <div className={`menu ${open ? 'visible transition' :''}`}>
             {renderedOptions}
             </div>
         </div>
