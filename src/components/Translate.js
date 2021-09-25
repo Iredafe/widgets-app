4340c5146e2ec74e5d82f369327a817b6aae26e8
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-09-24 22:10:39 -0600

    update

--- src/components/Translate.js
+++ src/components/Translate.js
@@ -17,10 +17,14 @@ const options = [
 ]
 
 const Translate=()=>{
-    const [language, setLanguage]= useState[0]
-return <div>
-<Dropdown options={options} selected={language} onSelectedChange={setLanguage}/>
-    </div>
+    const [language, setLanguage]= useState[options[0]]
+return 
+(<div>
+<Dropdown
+options={options} 
+selected={language} 
+onSelectedChange={setLanguage}/>
+</div>)
 
 }
 
