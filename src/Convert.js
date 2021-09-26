c19ba79270ea59a0ea9df8c26239da86a1697761
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-09-25 21:16:07 -0600

    making it work

--- src/Convert.js
+++ src/Convert.js
@@ -2,15 +2,19 @@ import axios from "axios";
 import React, {useState, useEffect} from "react";
 
 const Convert=({language, text})=>{
-const [newText, setNewText]=useState('')
+const [translation, setTranslation]=useState('');
     useEffect(()=>{
-        axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
-            params:{
-                q:text,
-                target:language.value,
-                key:'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
-            }
-        })
+        const doTranslation = async()=>{
+            const {data} = await  axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                params:{
+                    q:text,
+                    target:language.value,
+                    key:'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+                }
+            });
+
+            setTranslation(data.data.translations[0].translatedText);
+        }      
     },[language, text]);
 
 return <div/>
