66a63d289ae55a329a2a363597d9f96d9726aff4
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-09-28 22:37:13 -0600

    update url from event handler

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 const Link=({className, href, children})=>{
+    
+    const onClick=(event)=>{
+        event.preventDefault();
+        window.history.pushState({}, '', href);
+    }
+
     return (
         <div>
-            <a className={className} href={href}>
+            <a onClick={onClick} className={className} href={href}>
                 {children}
              </a>
         </div>
