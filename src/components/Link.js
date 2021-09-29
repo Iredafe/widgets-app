1cfab3ab9be2fc0706cc75a618948b0fd2706e6f
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-09-28 23:05:07 -0600

    add & remove event listener

--- src/components/Link.js
+++ src/components/Link.js
@@ -5,6 +5,11 @@ const Link=({className, href, children})=>{
     const onClick=(event)=>{
         event.preventDefault();
         window.history.pushState({}, '', href);
+
+        const navEvent = new PopStateEvent('popstate');
+        window.dispatchEvent(navEvent);
+
+        return
     }
 
     return (
