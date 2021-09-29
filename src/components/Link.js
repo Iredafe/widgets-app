f843d7e720c9c47c5bd47f8a7dc2e7ad40e5e424
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-09-28 22:21:35 -0600

    Building link closes #27

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,9 +1,11 @@
 import React from "react";
 
-const Link=()=>{
+const Link=({className, href, children})=>{
     return (
         <div>
-            
+            <a className={className} href={href}>
+                {children}
+             </a>
         </div>
     )
 }
