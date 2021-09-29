d1885f8cca8622d47276f741539aec68e9fffeb7
Iredafe Owolabi <iredafeowolabi@gmail.com>
2021-09-28 21:34:04 -0600

    building component

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,17 @@ import React from "react";
 const Header=()=>{
     return(
         <div className="ui secondary pointing menu">
-
+            <a href="/" className="item">
+               Accordion
+            </a>
+            <a href="/" className="item">
+               Search
+            </a>
+            <a href="/" className="item">
+               Dropdown
+            </a><a href="/" className="item">
+               Translate
+            </a>
         </div>
     )
 };
